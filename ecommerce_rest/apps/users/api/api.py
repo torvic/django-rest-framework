1a0eedb097bdd12599ef7394c3a0f230812b4ccc
torvic <130111@unsaac.edu.pe>
2021-04-30 17:55:25 -0500

    serializer & apiview

---
+++ ecommerce_rest/apps/users/api/api.py
@@ -0,0 +1,15 @@
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.decorators import api_view
+from apps.users.models import User
+from apps.users.api.serializer import UserSerializer
+
+@api_view(['GET','POST'])
+def user_api_view(request):
+    
+    if request.method == 'GET':
+        users = User.objects.all()
+        users_serializer = UserSerializer(users, many=True)
+        return Response(users_serializer.data)
+    elif request.method == 'POST':
+        print(request.data)
