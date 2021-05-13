2c4a3f7c339210fe99c3b7f9fad22c20da6ad7e8
torvic <130111@unsaac.edu.pe>
2021-05-13 16:36:01 -0500

    APIView

---
+++ ecommerce_rest/apps/products/api/views/general_views.py
@@ -0,0 +1,44 @@
+from rest_framework import generics
+
+from apps.products.models import MeasureUnit, CategoryProduct, Indicator
+from apps.products.api.serializers.general_serializers import MeasureUnitSerializer, IndicadorSerializer, CategoryProductSerializer
+
+class MeasureUnitListAPIView(generics.ListAPIView):
+  serializer_class = MeasureUnitSerializer
+
+  def get_queryset(self):
+    return MeasureUnit.objects.filter(state=True)
+
+class IndicatorListAPIView(generics.ListAPIView):
+  serializer_class = IndicadorSerializer
+
+  def get_queryset(self):
+    return Indicator.objects.filter(state=True)
+
+class CategoryProductListAPIView(generics.ListAPIView):
+  serializer_class = CategoryProductSerializer
+
+  def get_queryset(self):
+    return CategoryProduct.objects.filter(state=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
