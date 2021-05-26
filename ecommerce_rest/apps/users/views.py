714670239ee3ea298f720ebbf3d6958291f15666
torvic <130111@unsaac.edu.pe>
2021-05-26 17:18:02 -0500

    logout

--- ecommerce_rest/apps/users/views.py
+++ ecommerce_rest/apps/users/views.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from django.contrib.sessions.models import Session
 from rest_framework import status
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
 from rest_framework.authtoken.views import ObtainAuthToken
@@ -30,8 +31,33 @@ class Login(ObtainAuthToken):
       else:
         return Response({'error':'Este usuario no puede iniciar sesion'}, status=status.HTTP_401_UNAUTHORIZED)
     else:
-      return Response({'error':'Nombre de Usuario o contraseña incorrectos.'}, status=status.HTTP)
+      return Response({'error':'Nombre de Usuario o contraseña incorrectos.'}, status=status.HTTP_409_CONFLICT)
     return Response({'message':"Hola desde response"}, status=status.HTTP_200_OK)
+
+class Logout(APIView):
+  def get(self, request, *args, **kwargs):
+    try:
+      token = request.GET.get('token')
+      print(token)
+      token = Token.objects.filter(key=token).first()
+      if token:
+        user = token.user
+        all_sessions = Session.objects.filter(expire_date__gte=datetime.now())
+        if all_sessions.exists():
+          for session in all_sessions:
+            session_data = session.get_decoded() 
+            if user.id == int(session_data.get('_auth_user_id')):
+              session.delete()
+        token.delete()
+
+        session_message = 'sesiones de usuario eliminadas.'
+        token_message = 'token eliminado'
+
+        return Response({'token_message': token_message, 'session_message': session_message}, status=status.HTTP_200_OK)
+      return Response({'error':'No se ha encontrado un usuario con estas creadenciales'}, status=status.HTTP_400_BAD_REQUEST)
+    except:
+      return Response({'error': 'No se ha encontrado token en la peticion'}, status=status.HTTP_409_CONFLICT)
+
 
 
 
